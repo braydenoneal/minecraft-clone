4f3a369628bef55847ad060afd3da6362b0af336
Brayden O'Neal <braydenioneal@gmail.com>
2024-03-13 12:07:03 -0500

    Better camera controls

--- src/input/input.hpp
+++ src/input/input.hpp
@@ -25,6 +25,13 @@ namespace input {
         }
     }
 
+    glm::vec3 translate_in_direction_by_amount(glm::vec3 position, float angle, glm::vec3 amount) {
+        glm::mat4 rotate_mat = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::vec3 rotated_amount = rotate_mat * glm::vec4(amount, 1.0f);
+        glm::mat4 translation = glm::translate(glm::mat4(1.0f), rotated_amount);
+        return translation * glm::vec4(position, 1.0f);
+    }
+
     void poll_events() {
         glfwPollEvents();
         double current_x;
@@ -35,17 +42,26 @@ namespace input {
         window::cursor_x = current_x;
         window::cursor_y = current_y;
 
-        matter::camera_angle.y += (float) window::cursor_difference_x / ((float) M_PI * 200.0f);
         matter::camera_angle.x += (float) window::cursor_difference_y / ((float) M_PI * 200.0f);
+        matter::camera_angle.y += (float) window::cursor_difference_x / ((float) M_PI * 200.0f);
+
+        matter::camera_angle.x = std::max(-(float) M_PI / 2.0f, matter::camera_angle.x);
+        matter::camera_angle.x = std::min( (float) M_PI / 2.0f, matter::camera_angle.x);
 
         for (std::pair<const int, int> key : keys) {
             if (key.second) {
                 switch (key.first) {
                     case GLFW_KEY_A:
-                        matter::camera_position.x -= matter::camera_speed;
+                        matter::camera_position = translate_in_direction_by_amount(
+                            matter::camera_position, matter::camera_angle.y,
+                            glm::vec3(-matter::camera_speed, 0.0f, 0.0f)
+                        );
                         break;
                     case GLFW_KEY_D:
-                        matter::camera_position.x += matter::camera_speed;
+                        matter::camera_position = translate_in_direction_by_amount(
+                            matter::camera_position, matter::camera_angle.y,
+                            glm::vec3(matter::camera_speed, 0.0f, 0.0f)
+                        );
                         break;
                     case GLFW_KEY_SPACE:
                         matter::camera_position.y += matter::camera_speed;
@@ -54,10 +70,16 @@ namespace input {
                         matter::camera_position.y -= matter::camera_speed;
                         break;
                     case GLFW_KEY_S:
-                        matter::camera_position.z -= matter::camera_speed;
+                        matter::camera_position = translate_in_direction_by_amount(
+                            matter::camera_position, matter::camera_angle.y,
+                            glm::vec3(0.0f, 0.0f, -matter::camera_speed)
+                        );
                         break;
                     case GLFW_KEY_W:
-                        matter::camera_position.z += matter::camera_speed;
+                        matter::camera_position = translate_in_direction_by_amount(
+                            matter::camera_position, matter::camera_angle.y,
+                            glm::vec3(0.0f, 0.0f, matter::camera_speed)
+                        );
                         break;
                     default:
                         break;
