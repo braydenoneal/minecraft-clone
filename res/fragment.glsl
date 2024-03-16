3863a816c3ef058564638642c08d2a3a29e02f9f
Brayden O'Neal <braydenioneal@gmail.com>
2024-03-16 10:03:40 -0500

    Fix another ambient occlusion error

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -11,6 +11,6 @@ void main() {
     vec4 texture_color = texture(u_textures, v_texture_coordinates);
     float fog_distance = gl_FragCoord.z / gl_FragCoord.w;
     texture_color *= f_Brightness;
-    texture_color = mix(texture_color, vec4(138.0f / 255.0f, 163.0f / 255.0f, 1.0f, 1.0f), (1.0 - exp2(-0.0005 * fog_distance * fog_distance)));
+//    texture_color = mix(texture_color, vec4(138.0f / 255.0f, 163.0f / 255.0f, 1.0f, 1.0f), (1.0 - exp2(-0.0005 * fog_distance * fog_distance)));
     color = texture_color;
 }
