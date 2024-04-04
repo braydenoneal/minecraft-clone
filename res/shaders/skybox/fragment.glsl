d264ba60dbc4da38f810e60ac50b88b1a97f8cb1
Brayden O'Neal <braydenioneal@gmail.com>
2024-04-04 14:03:28 -0500

    Update sky fog values

--- res/shaders/skybox/fragment.glsl
+++ res/shaders/skybox/fragment.glsl
@@ -7,7 +7,7 @@ in vec3 fog_position;
 void main() {
     float dist = sqrt(pow(fog_position.x, 2) + pow(fog_position.y, 2) + pow(fog_position.z, 2));
     float start = 1;
-    float scale = 8;
+    float scale = 3;
     float factor = (clamp(dist, start, start + scale) - start) / scale;
     color = mix(vec4(121.0f / 255.0f, 166.0f / 255.0f, 1.0f, 1.0f),
                 vec4(178.0f / 255.0f, 206.0f / 255.0f, 1.0f, 1.0f), factor);
