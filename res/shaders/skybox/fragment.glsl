7047795efcaba2ee2b4f6c651d05907b275a948a
Brayden O'Neal <braydenioneal@gmail.com>
2024-04-03 22:58:13 -0500

    Change fog values

--- res/shaders/skybox/fragment.glsl
+++ res/shaders/skybox/fragment.glsl
@@ -5,5 +5,5 @@ layout (location = 0) out vec4 color;
 void main() {
     float fog_distance = gl_FragCoord.z / gl_FragCoord.w;
     color = mix(vec4(121.0f / 255.0f, 166.0f / 255.0f, 1.0f, 1.0f), vec4(178.0f / 255.0f, 206.0f / 255.0f, 1.0f, 1.0f),
-        (1.0 - exp2(-0.00000005 * fog_distance * fog_distance * fog_distance * fog_distance * fog_distance)));
+        (1.0 - exp2(-0.00005 * fog_distance * fog_distance * fog_distance)));
 }
